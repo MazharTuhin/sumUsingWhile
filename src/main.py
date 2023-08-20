721fc31270442dba9faeb67f2db0aa9e955598f5
root <root@localhost>
2023-08-20 10:43:41 +0600

    auto publish 1692506621308

---
+++ src/main.py
@@ -0,0 +1,18 @@
+#by apps
+i = 1
+sum = 0
+while i <= 10:
+  if i%2 == 0:
+    sum = sum + i
+  i = i + 1
+print(sum)
+  
+
+#by me
+i = 2 
+sum = 0
+
+while i <= 10:
+  sum = sum + i 
+  i = i + 2
+print(sum)
