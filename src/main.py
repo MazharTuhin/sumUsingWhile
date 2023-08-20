d67e9cd136f809d3bd39e4a3fb811a733cfcd2d6
root <root@localhost>
2023-08-20 10:47:30 +0600

    auto publish 1692506850617

--- src/main.py
+++ src/main.py
@@ -1,6 +1,11 @@
+#add odd numbers 1-50
+
 i = 1
 sum = 0
-while i <= 10:
-   sum = sum + i
-   i = i + 1
-print (sum)
+while i <= 50:
+  if i%2 != 0:
+    sum = sum + i
+  i = i + 1
+print(sum)
+  
+
